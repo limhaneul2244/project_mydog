256e9baa1f2882351e210ec8f5e321d38af1d595
hnlim <hnlim2244@gmail.com>
2021-05-17 22:33:05 +0900

    0517_slider, footer 작업

--- mainjs/main.js
+++ mainjs/main.js
@@ -9,16 +9,16 @@ $(document).ready(function () {
         $(".slide_img>li").eq(0).siblings().css({
             "left": '-1920px'
         }); //첫번째 li에 있는 형제 이외에는 왼쪽으로 이동
-        
-        setInterval(slide, 2000); // 2초마다 slide()함수 호출
+
+        setInterval(slide, 4000); // 2초마다 slide()함수 호출
     }
 
     function slide() { //슬라이드의 함수'
         // now 1증가
-        ++now; 
+        ++now;
 
         // now가 imgs보다 크거나, 같으면 now를 0으로(다시처음으로) 바꾼다.
-        if (now >= imgs) { 
+        if (now == imgs) {
             now = 0;
         }
 
@@ -41,8 +41,7 @@ $(document).ready(function () {
                 "left": '-1920px',
                 "transition": 'all 0s'
             });
-        }
-        else { // now가 0보다 클때
+        } else { // now가 0보다 클때
             // now - 1 (앞에 있는) li는 0.8초동안 오른쪽으로 이동.
             $(".slide_img>li").eq(now - 1).css({
                 "left": '1920px',
@@ -56,4 +55,60 @@ $(document).ready(function () {
             });
         }
     }
+});
+
+// 숫자 카운팅 효과
+
+$(function() {
+  var count0 = count1 = count2 = count3 = 0;
+
+  timeCounter();
+
+  function timeCounter() {
+
+    // id0 = setInterval(count0Fn, 12.738853);
+
+    // function count0Fn() {
+    //   count0++;
+    //   if (count0 > 785) {
+    //     clearInterval(id0);
+    //   } else {
+    //     $(".point_red_txt").eq(0).text(count0);
+    //   }
+
+    // }
+
+    id1 = setInterval(count1Fn, 0.5);
+
+    function count1Fn() {
+      count1++;
+      if (count1 > 1994) {
+        clearInterval(id1);
+      } else {
+        $(".point_red_txt").eq(1).text(count1);
+      }
+    }
+
+    id2 = setInterval(count2Fn, 200.57142857);
+
+    function count2Fn() {
+      count2++;
+      if (count2 > 12) {
+        clearInterval(id2);
+      } else {
+        $(".point_red_txt").eq(2).text(count2);
+      }
+    }
+
+    id3 = setInterval(count3Fn, 200.57142857);
+
+    function count3Fn() {
+      count3++;
+      if (count3 > 15) {
+        clearInterval(id3);
+      } else {
+        $(".point_red_txt").eq(3).text(count3);
+      }
+    }
+  }
 });
