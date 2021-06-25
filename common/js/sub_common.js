66c3e1d5194c655049618a37066f5630b1174c1c
hnlim <hnlim2244@gmail.com>
2021-06-25 22:44:48 +0900

    sub_page 일부 개선

--- common/js/sub_common.js
+++ common/js/sub_common.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
     $(".sub_level01>.submenu").toggle(
         function(){ //첫번째 기능
         $(".section01").fadeIn('fast');
-        $(".arrow_icon").css({'background-image':"url(../images/arrow_up.png)"});
+        $(".arrow_icon").css({'background-image':"url(../common/images/arrow_up.png)"});
 
     }, function(){ //두번째 기능
         $(".section01").fadeOut('fast');
-        $(".arrow_icon").css({'background-image':"url(../images/arrow_down.png)"});
+        $(".arrow_icon").css({'background-image':"url(../common/images/arrow_down.png)"});
     });
 });
 
@@ -15,10 +15,10 @@ $(document).ready(function(){
     $(".sub_level02>.submenu01").toggle(
         function(){ //첫번째 기능
         $(".section02").fadeIn('fast');
-        $(".arrow_icon02").css({'background-image':"url(../images/arrow_up.png)"});
+        $(".arrow_icon02").css({'background-image':"url(../common/images/arrow_up.png)"});
 
     }, function(){ //두번째 기능
         $(".section02").fadeOut('fast');
-        $(".arrow_icon02").css({'background-image':"url(../images/arrow_down.png)"});
+        $(".arrow_icon02").css({'background-image':"url(../common/images/arrow_down.png)"});
     });
 });
